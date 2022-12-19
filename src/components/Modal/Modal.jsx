8cbb3f319ec69f03d9b58c0c9052250eb9b9e136
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-12-19 04:36:09 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,20 +7,6 @@ import { Overlay, ModalStyled, BtnClose } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-// export class Modal extends Component {
-// static propTypes = {
-//   onClick: PropTypes.func.isRequired,
-//   onClose: PropTypes.func.isRequired,
-//   children: PropTypes.node.isRequired,
-// };
-
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//   }
 export function Modal({ onClose, children, prevChildren, nextChildren }) {
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
@@ -32,21 +18,17 @@ export function Modal({ onClose, children, prevChildren, nextChildren }) {
   const handleKeyDown = e => {
     if (e.code === 'Escape') {
       onClose();
-      // this.props.onClose();
     }
   };
 
   const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
       onClose();
-      // this.props.onClose();
     }
   };
 
-  // render() {
   return createPortal(
     <Overlay onClick={handleBackdropClick}>
-      {/* <ModalStyled>{this.props.children}</ModalStyled> */}
       <ModalStyled>{children}</ModalStyled>
       <BtnClose type="button" onClick={onClose}>
         <GrClose style={{ width: 30, height: 30, margin: 0 }} />
@@ -54,7 +36,6 @@ export function Modal({ onClose, children, prevChildren, nextChildren }) {
     </Overlay>,
     modalRoot
   );
-  // }
 }
 
 Modal.propTypes = {
